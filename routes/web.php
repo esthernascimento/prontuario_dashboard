045e59e36b71dfc9cf69b2bd0b38499331f3e5c1
esthernascimento <esthersolzinhacarambola@gmail.com>
2025-08-14 14:10:00 -0300

    add de imagens e login

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::get('/cadastro', function () {
+    return view('cadastro');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
 });
