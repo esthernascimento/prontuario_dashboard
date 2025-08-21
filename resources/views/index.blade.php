ad1c6ee27eabfaf6b120cbb21cf088cc54d4786d
ks10x <kaua160308@gmail.com>
2025-08-21 17:01:41 -0300

    criação tela inicial/css

---
+++ resources/views/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Prontuário+</title>
+  
+  <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+  
+</head>
+
+<body>
+    <main class="main-container">
+
+        <div class="box-content">
+            <div class="content">
+                <h1>Seja Bem-Vindo ao Prontuário+</h1>
+
+                <button>SOU <p>ADMINISTRADOR</p></button>
+
+                <button>SOU <p>ENFERMEIRO</p></button>
+
+                <button>SOU <p>MÉDICO</p></button>
+            </div>
+
+            <div class="img-content">
+                <img src="{{asset('img/logo-azul.png')}}" class="logo">
+            </div>
+
+        </div>
+
+        
+
+       
+
+    </main>
+</body>
+
+</html>
