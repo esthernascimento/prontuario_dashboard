6265e712afe0ae357805f799bbbd79c2afca12bd
barbarassss <barbaraa.suzi@gmail.com>
2025-09-16 17:42:29 -0300

    criação template geral

---
+++ resources/views/geral/templates/geralTemplate.blade.php
@@ -0,0 +1,39 @@
+<link rel="stylesheet" href="{{ asset('css/admin/dashboardAdm.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/geral/template.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/geral/pacientes.css') }}">
+
+  @include('geral.templates.sidebar')
+  @include('geral.templates.header')
+
+<!-- sidebar -->
+<div class="sidebar">
+    <a href="{{ route('admin.perfil') }}">
+      <img src="{{ asset('img/adm-logo2.png') }}" alt="Logo Prontuário+" class="logo">
+    </a>
+    <nav>
+      <a href="{{ route('admin.dashboard') }}"><i class="bi bi-house-door-fill"></i></a>
+      <a href="{{ route('admin.pacientes') }}"><i class="bi bi-people-fill"></i></a>
+      <a href="{{ route('admin.manutencaoMedicos') }}"><i class="bi bi-plus-circle-fill"></i></a>
+      <a href="{{ route('admin.ajuda') }}"><i class="bi bi-question-circle-fill"></i></a>
+      <a href="{{ route('admin.seguranca') }}"><i class="bi bi-shield-lock-fill"></i></a>
+      <a href="{{ route('admin.logout') }}"
+        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+        <i class="bi bi-power"></i>
+      </a>
+      <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" style="display: none;">
+        @csrf
+      </form>
+    </nav>
+  </div>
+
+  <!-- header  -->
+  <header class="header">
+      <a href="{{ route('admin.perfil') }}" class="user-info" style="text-decoration: none; color: inherit;">
+        @if($admin && $admin->foto)
+        <img src="{{ asset('storage/fotos/' . $admin->foto) }}" alt="Foto do Admin">
+        @else
+        <img src="{{ asset('img/usuario-de-perfil.png') }}" alt="Foto padrão">
+        @endif
+        <span>{{ $admin->nomeAdmin ?? 'Administrador' }}</span>
+      </a>
+    </header>
